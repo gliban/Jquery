52b5c11798b8bb0df47a41aec43633fdb34c9eeb
gliban <galgallo85@gmail.com>
2017-05-31 16:07:47 +0300

    Jquery

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function() {
+    jQuery("h1").click(function() {
+        alert("This is Burhan kichwa kubwa.");
+    });
+
+    jQuery("p").click(function() {
+        alert("This is Burhan superpower.");
+    });
+
+    jQuery("img").click(function() {
+        alert("This is Burhan Garre.");
+    });
+});
